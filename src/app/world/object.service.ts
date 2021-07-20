6952c762b9d99ec19ebfe6b3cb5e3ed50b11265a
Blaxar Waldarax <blaxar.waldarax@gmail.com>
2021-07-20 23:38:41 +0200

    WIP: InstancedMesh usage for world objects

--- src/app/world/object.service.ts
+++ src/app/world/object.service.ts
@@ -1,9 +1,79 @@
 import {Injectable} from '@angular/core'
 import {HttpService} from './../network/http.service'
 import RWXLoader from 'three-rwx-loader'
-import {Group, Mesh, ConeGeometry, LoadingManager, MeshBasicMaterial, RepeatWrapping, TextureLoader} from 'three'
+import {Group, Mesh, InstancedMesh, ConeGeometry, LoadingManager, MeshBasicMaterial, RepeatWrapping, TextureLoader, Matrix4} from 'three'
 import * as JSZip from 'jszip'
 import JSZipUtils from 'jszip-utils'
+
+class CachedObject {
+
+  private original : Mesh // Original instance of the loaded object.
+  private instanced : InstancedMesh // InstancedMesh for the object, allows for efficient spawning of multiple instances
+  private globalToLocalIdMap : Map<number, number>
+  private availableSlots : Set<number>
+  private counter : number = 0
+
+  constructor( object: Mesh, count: number) {
+    this.original = object
+    this.instanced = new InstancedMesh(this.original.geometry, this.original.material, count)
+  }
+
+  // Spawn a new instance of this type
+  add(id: number, transform: Matrix4): boolean {
+    if(this.availableSlots.size > 0) {
+      const availableID = this.availableSlots.keys()[0]
+      this.instanced.setMatrixAt(availableID, transform);
+      this.instanced.instanceMatrix.needsUpdate = true;
+      this.availableSlots.delete(availableID);
+
+      return true;
+    }
+
+    // If the maximum number of instances has been reached: we cannot add more
+    if(this.counter >= this.instanced.count)
+      return false;
+
+    this.globalToLocalIdMap.set(id, this.counter)
+    this.instanced.setMatrixAt(this.counter++, transform);
+
+    return true;
+  }
+
+  // Update an existing instance of this type
+  update(id: number, transform: Matrix4): boolean {
+    // If there's no object with de provided id: we cannot do anything
+    if(!this.globalToLocalIdMap.has(id))
+      return false;
+
+    const localId = this.globalToLocalIdMap.get(id)
+
+    this.instanced.setMatrixAt(localId, transform);
+
+    return true;
+  }
+
+  // Remove an exitsing object of this type
+  remove(id: number): boolean {
+    // If there's no object with de provided id: we cannot do anything
+    if(!this.globalToLocalIdMap.has(id))
+      return false;
+
+    const localId = this.globalToLocalIdMap.get(id)
+    let transform: Matrix4 = new Matrix4()
+
+    // By filling the transformation matrix with zeros: we disable this instance
+    transform.set(0, 0, 0, 0,
+                  0, 0, 0, 0,
+                  0, 0, 0, 0,
+                  0, 0, 0, 0);
+    this.instanced.setMatrixAt(localId, transform);
+    this.instanced.instanceMatrix.needsUpdate = true;
+
+    this.globalToLocalIdMap.delete(localId)
+
+    return true;
+  }
+}
 
 @Injectable({providedIn: 'root'})
 export class ObjectService {
@@ -13,6 +83,7 @@ export class ObjectService {
   private objects: Map<string, Promise<any>> = new Map()
   private textures: Map<string, any> = new Map()
   private path = 'http://localhost'
+  private defaultCount: number = 1000
 
   constructor(private http: HttpService) {
     const cone = new Mesh(new ConeGeometry(0.5, 0.5, 3), new MeshBasicMaterial({color: 0x000000}))
@@ -23,7 +94,7 @@ export class ObjectService {
 
   setPath(path: string) {
     this.path = path
-    this.rwxLoader.setPath(`${this.path}/rwx`).setResourcePath(`${this.path}/textures`)
+    this.rwxLoader.setPath(`${this.path}/rwx`).setResourcePath(`${this.path}/textures`).setFlatten(true)
   }
 
   loadAvatars() {
@@ -49,7 +120,7 @@ export class ObjectService {
       return this.objects.get(name)
     } else {
       const promise = new Promise((resolve, reject) => {
-        this.rwxLoader.load(name, (rwx: Group) => resolve(rwx), null, () => resolve(this.errorCone))
+        this.rwxLoader.load(name, (rwx: Mesh) => resolve(new CachedObject(rwx, this.defaultCount)), null, () => resolve(this.errorCone))
       })
       this.objects.set(name, promise)
       return promise
