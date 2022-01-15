bdfb845db425c603b1dac0ea5f13d28aa0a78e26
Blaxar Waldarax <blaxar.waldarax@gmail.com>
2022-01-15 15:34:42 +0100

    WIP

--- docker/start_lemuria.sh
+++ docker/start_lemuria.sh
@@ -1,5 +1,12 @@
-[Unit]
-Description=Workaround to serve static web browser files on Lemuria without triggering CORS issues
+cd /root/lemuria
+npm run build
 
-[Service]
-ExecStart=/bin/bash -c 'cd /root/lemuria/srv && python3 tools/serve_path.py'
+cd srv
+python3 tools/serve_path.py &
+python3 app.py &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of the first-exited process
+exit $?
